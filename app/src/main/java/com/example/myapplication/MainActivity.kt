57d1092c52275284ee514091c5ab104e5d74d149
sayan <sayankarmakar09990@gmail.com>
2024-04-22 18:59:33 +0530

    scroll view

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.myapplication
 
+import android.health.connect.datatypes.units.Length
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.Switch
 import android.widget.Toast
+import android.widget.ToggleButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +25,19 @@ class MainActivity : AppCompatActivity() {
 //        }
 //        light.setOnClickListener {
 //            layout.setBackgroundResource(R.color.yellow)
+//        }
+//        val toggle = findViewById<Switch>(R.id.toggleSwitch)
+//        val layout = findViewById<RelativeLayout>(R.id.mainLayout)
+//
+//
+//        toggle.setOnCheckedChangeListener { buttonView, isChecked ->
+//            if (isChecked) {
+//                layout.setBackgroundResource(R.color.yellow)
+//                Toast.makeText(applicationContext,"changing to yellow mode....",Toast.LENGTH_SHORT).show()
+//            } else {
+//                layout.setBackgroundResource(R.color.white)
+//                Toast.makeText(applicationContext,"changing to normal mode..",Toast.LENGTH_SHORT).show();
+//            }
 //        }
     }
 }
