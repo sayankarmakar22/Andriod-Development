6963d406bfb73610a4225c6a1f458a716b80f2ad
Sayan Karmakar <106605492+sayankarmakar22@users.noreply.github.com>
2024-05-07 19:58:17 +0530

    Firebase CURD operation using firebase realtime database

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,43 +1,66 @@
-package com.example.myapplication
+package com.example.uiux
 
-import android.health.connect.datatypes.units.Length
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.RelativeLayout
-import android.widget.Switch
 import android.widget.Toast
-import android.widget.ToggleButton
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
+import java.sql.SQLData
 
 class MainActivity : AppCompatActivity() {
+
+//    lateinit var database: FirebaseFires
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val email = findViewById<TextInputEditText>(R.id.userEmail)
+        val name = findViewById<TextInputEditText>(R.id.userName)
+        val register = findViewById<Button>(R.id.register)
+
+
+        register.setOnClickListener {
+            val  enteredEmail = email.text.toString()
+            val  enteredName = name.text.toString()
+
+            val db = FirebaseDatabase.getInstance().getReference("Users")
+            val regisUser = RegisteredUsers(enteredName,enteredEmail)
+
+            //create
+            db.child(enteredName).setValue(regisUser).addOnSuccessListener {
+                Toast.makeText(this,"Registration Done!",Toast.LENGTH_SHORT).show()
+            }
+                //read
+//            db.child(enteredName).get().addOnSuccessListener {
+//                if(it.exists()){
+//                    val name = it.child("name").value
+//                    val email = it.child("email").value
 //
-//        val dark = findViewById<Button>(R.id.dark)
-//        val light = findViewById<Button>(R.id.light)
-//        val layout = findViewById<LinearLayout>(R.id.linearLayout)
-//
-//        dark.setOnClickListener {
-//            layout.setBackgroundResource(R.color.black)
-//        }
-//        light.setOnClickListener {
-//            layout.setBackgroundResource(R.color.yellow)
-//        }
-//        val toggle = findViewById<Switch>(R.id.toggleSwitch)
-//        val layout = findViewById<RelativeLayout>(R.id.mainLayout)
-//
-//
-//        toggle.setOnCheckedChangeListener { buttonView, isChecked ->
-//            if (isChecked) {
-//                layout.setBackgroundResource(R.color.yellow)
-//                Toast.makeText(applicationContext,"changing to yellow mode....",Toast.LENGTH_SHORT).show()
-//            } else {
-//                layout.setBackgroundResource(R.color.white)
-//                Toast.makeText(applicationContext,"changing to normal mode..",Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(this,"name : "+ name + " " + "email : "+ email ,Toast.LENGTH_LONG) .show()
+//                }
 //            }
-//        }
+
+            //update
+//            val updatedUser = mapOf<String,String>(
+//                "name" to enteredName,
+//                "email" to enteredEmail
+//            )
+//            db.child(enteredName).updateChildren(updatedUser).addOnSuccessListener {
+//                Toast.makeText(this,"Updated " ,Toast.LENGTH_LONG) .show()
+//            }
+
+            //delete
+
+//            db.child(enteredName).removeValue().addOnSuccessListener {
+//                Toast.makeText(this,"Deleted " ,Toast.LENGTH_LONG) .show()
+//            }
+        }
+
     }
 }
